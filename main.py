9e2a7ea551f524b5342b77ed61340b2abdb175a0
pritul2 <pritul.dave@gmail.com>
2022-01-19 19:09:10 +0530

    Pseudo Class

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import numpy as np
+import re
+import random
+import string
 import asyncio
+from strgen import StringGenerator as SG
 
-print("Testing")
+class Pseudo:
+    def __init__(self):
+        pass
+    def generate_rand_string(self):
+        self.rand_num = np.random.randint(low=3,high=10)
+        self.rand_pattern = "[A-Z]{"+str(self.rand_num)+"}"
+        return SG(self.rand_pattern).render()
+
+pseu_obj = Pseudo()
+print(pseu_obj.generate_rand_string())
